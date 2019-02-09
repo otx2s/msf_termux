26fa133f6465c76d4ca6a404fc2f636b5f6196e8
otx2s <47149411+otx2s@users.noreply.github.com>
2019-02-09 17:58:40 +0700

    Update msfter.py

--- msfter.py
+++ msfter.py
@@ -19,31 +19,36 @@ dMP dMP dMP VMMMP" dMP        dMP   dMMMMMP dMP dMP
 def logo():
 	print msfter_logo
 
+def install():
+	os.system ("pkg update -y")
+	os.system ("cd $HOME && git clone https://github.com/Hax4us/Metasploit_termux")
+	os.system ("cd $HOME/Metasploit_termux && chmod +x metasploit.sh")
+	os.system ("cd $HOME/Metasploit_termux && bash metasploit.sh")
+	os.system ("cd $HOME && bundle init > hdh.txt && rm -rf hdh.txt")
+	os.system ("cd $HOME && gem install bundler -v 1.16.1")
+	os.system ("cd $HOME && bundle install -j5")
+	os.system ("cd $HOME/Metasploit_termux && bash metasploit.sh ")
+
 def main():
 	logo()
 	fff = raw_input("msfter > ")
 	if fff == "1" or fff == "01":
-		print "======================================"
+		print "\n======================================"
 		print "[+] Jesus, it will take a lot of time!"
 		print "======================================"
-		os.system ("pkg update -y")
-		os.system ("cd $HOME && git clone https://github.com/Hax4us/Metasploit_termux")
-		os.system ("cd $HOME/Metasploit_termux && chmod +x metasploit.sh")
-		os.system ("cd $HOME/Metasploit_termux && bash metasploit.sh")
-		os.system ("cd $HOME && bundle init > hdh.txt && rm -rf hdh.txt")
-		os.system ("cd $HOME && gem install bundler -v 1.16.1")
-		os.system ("cd $HOME && bundle install -j5")
-		os.system ("cd $HOME/Metasploit_termux && bash metasploit.sh")
+		install()
 		print "====================================="
 		print "[+] Metasploit installed"
 		print "[+] Type 'msfconsole' to start."
 		print "====================================="
+		os.system("clear")
 
-	elif fff == "2":
+	elif fff == "2" or fff == "02":
 		print """
 		____________________________
 		||========================||
 		||    Created By otx2s    ||
+		||    ----------------    ||
 		||From podval with love :3||
 		||========================||
 		||                        ||
@@ -53,15 +58,19 @@ def main():
 		os.system("clear")
 		main()
 
-	elif fff == "3":
+	elif fff == "3" or fff == "03":
 		print "\nGoodbye..."
 		sl(1)
 		
 	else:
-		print "\nSomething wrong"
+		print "\nERROR: Check your command!"
 		sl(1)
 		os.system("clear")
 		main()
 
 if __name__ == '__main__':
 	main()
+
+####################
+# Privet, kak dela?#
+####################
