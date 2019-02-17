67a8a342101c76df73bf00693564cfdb2b46bdfe
otx2s <47149411+otx2s@users.noreply.github.com>
2019-02-17 22:52:57 +0700

    Update msfter.py

--- msfter.py
+++ msfter.py
@@ -40,7 +40,7 @@ def install():
 	os.system("cd $HOME && gem install bundler -v 1.16.1")
 	os.system("cd $HOME && bundle install -j5")
 	os.system("cd $HOME && bash metasploit.sh ")
-	os.system("cd $HOME && rm -rf README.md apk.rb database.yml metasploit.sh Metasploit_termux")
+	os.system("cd $HOME && rm -rf README.md apk.rb database.yml Metasploit_termux")
 	
 
 def main():
